936f69a6313d38bad1bb39771832bb7c68faf5ef
alstn2468 <alstn2468_@naver.com>
2018-05-21 12:27:24 +0900

    2018-05-21

---
+++ Go_Base/Chapter.3/variable.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+	// 변수의 선언
+	// var a int
+	//
+	// 변수의 선언 및 초기값 할당
+	// var f float32 = 11.
+	//
+	// 해당 타입의 값 할당
+	// a = 10
+	// f = 12.0
+	//
+	// 동일한 타입의 변수가 복수 개 있을 경우
+	// var i, j, k int = 1, 2, 3
+	//
+	// 상수의 선언
+	// const c int = 10
+	// const s string = "Hi"
+	//
+	// 자동으로 타입을 추론
+	// const c = 10
+	// const s = "Hi"
+	//
+	// 여러 개의 상수들을 묶어서 지정
+	// const (
+	// 	Visa   = "Visa"
+	// 	Master = "MasterCard"
+	// 	Amex   = "American Express"
+	// )
+	//
+	// 상수 값을 순차적으로 부여
+	// const (
+	// 	Apple  = iota // 0
+	// 	Grape         // 1
+	// 	Orange        // 2
+	// )
+}
