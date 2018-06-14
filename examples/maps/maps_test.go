07a8a8b3a71c7db366d8843a8cf24cbc100e5064
alstn2468 <alstn2468_@naver.com>
2018-06-14 16:49:05 +0900

    2018-06-14

--- examples/maps/maps_test.go
+++ examples/maps/maps_test.go
@@ -30,6 +30,18 @@ func TestCount_if(t *testing.T) {
 	}
 }
 
+func ExampleCount_no_sort() {
+	codeCount := map[rune]int{}
+	count("가나다나", codeCount)
+	for _, key := range []rune{'가', '나', '다'} {
+		fmt.Println(string(key), codeCount[key])
+	}
+	// Output:
+	// 가 1
+	// 나 2
+	// 다 1
+}
+
 func ExampleCount_sort() {
 	codeCount := map[rune]int{}
 	count("가나다나", codeCount)
